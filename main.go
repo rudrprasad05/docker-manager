a7f7f888662609dadffe594fe19335beafe1f1a7
Rudr <rudrprasad@yahoo.com>
2024-11-25 22:57:10 +1200

    new day?

--- main.go
+++ main.go
@@ -2,156 +2,26 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
 
-	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
-	"github.com/gorilla/mux"
 )
 
-var (
-	dockerClient *client.Client
-	uploadDir    = "/path/to/uploaded/images" // Ensure Docker can access this directory
-)
-
-func init() {
-	var err error
-	dockerClient, err = client.NewClientWithOpts(client.FromEnv)
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
-}
+	defer cli.Close()
 
-func uploadImage(w http.ResponseWriter, r *http.Request) {
-	file, header, err := r.FormFile("image")
+	containers, err := cli.ImageList(ctx, image.ListOptions{})
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	filePath := filepath.Join(uploadDir, header.Filename)
-	out, err := os.Create(filePath)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		panic(err)
 	}
 
-	// Load the image into Docker
-	imageFile, err := os.Open(filePath)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	for _, container := range containers {
+		fmt.Println(container.ID)
 	}
-	defer imageFile.Close()
-
-	_, err = dockerClient.ImageLoad(context.Background(), imageFile, true)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Image uploaded and loaded into Docker"})
-}
-
-func listImages(w http.ResponseWriter, r *http.Request) {
-	images, err := dockerClient.ImageList(context.Background(), types.ImageListOptions{})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(images)
-}
-
-func listContainers(w http.ResponseWriter, r *http.Request) {
-	containers, err := dockerClient.ContainerList(context.Background(), types.ContainerListOptions{All: true})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	json.NewEncoder(w).Encode(containers)
-}
-
-func deleteImage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	imageID := vars["id"]
-
-	_, err := dockerClient.ImageRemove(context.Background(), imageID, types.ImageRemoveOptions{Force: true})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Image deleted"})
-}
-
-func startContainer(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	containerID := vars["id"]
-
-	err := dockerClient.ContainerStart(context.Background(), containerID, types.ContainerStartOptions{})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Container started"})
-}
-
-func stopContainer(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	containerID := vars["id"]
-
-	err := dockerClient.ContainerStop(context.Background(), containerID, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Container stopped"})
-}
-
-func deleteContainer(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	containerID := vars["id"]
-
-	err := dockerClient.ContainerRemove(context.Background(), containerID, types.ContainerRemoveOptions{Force: true})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Container deleted"})
-}
-
-func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/images", uploadImage).Methods("POST")
-	r.HandleFunc("/images", listImages).Methods("GET")
-	r.HandleFunc("/images/{id}", deleteImage).Methods("DELETE")
-	r.HandleFunc("/containers", listContainers).Methods("GET")
-	r.HandleFunc("/containers/{id}/start", startContainer).Methods("POST")
-	r.HandleFunc("/containers/{id}/stop", stopContainer).Methods("POST")
-	r.HandleFunc("/containers/{id}", deleteContainer).Methods("DELETE")
-
-	fmt.Println("Server running on port 8080")
-	http.ListenAndServe(":8080", r)
 }
