5d1404e1c4efe418d06f1530d766ed4c7541ef60
Rudr <rudrprasad@yahoo.com>
2024-11-26 09:44:46 +1200

    feat: testing product viability

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 	"github.com/rudrprasad05/go-logs/logs"
+
+	"github.com/docker/docker/api/types/container"
 )
 
 func main() {
@@ -20,18 +24,30 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/docker/images/list", handle)
-	// mux.HandleFunc("/convert", routes.ConvertFile)
+	mux.HandleFunc("/docker/container/run", func(w http.ResponseWriter, r *http.Request) {
+		imageName := "go-http-server"
+		containerName := "go-http-server-container"
+		cmd := []string{"./main"}
+		hostPort := "8082"
+		containerPort := "8082"
+	
+		// Create and run the container
+		createAndRunContainer(imageName, containerName, cmd, hostPort, containerPort)
+	
+	})
 	// mux.HandleFunc("/download", routes.DownloadImageHandler)
 
 	corsHandler := enableCORS(mux)
 	loggedHandler := logs.LoggingMiddleware(logger, corsHandler)
 
+	
 
 	log.Println("Server running on port 8081...")
 	log.Fatal(http.ListenAndServe(":8081", loggedHandler))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
+	var imgArr []string
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
@@ -47,12 +63,15 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	for _, image := range images {
 		if len(image.RepoTags) > 0 {
 			for _, tag := range image.RepoTags {
-				fmt.Println(tag)
+				imgArr = append(imgArr, tag)
 			}
 		} else {
 			fmt.Println("<untagged image>")
 		}
 	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(imgArr)
 }
 
 func enableCORS(next http.Handler) http.Handler {
@@ -66,4 +85,36 @@ func enableCORS(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func createAndRunContainer(imageName string, containerName string, cmd []string, hostPort string, containerPort string) (string, error){
+	ctx := context.Background()
+
+	// Create a Docker client
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	// Configure the container
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: imageName, // Use the local image
+		Cmd:   cmd,       // Command to execute
+		Tty:   false,     // Do not allocate a TTY
+	}, &container.HostConfig{
+		PortBindings: map[nat.Port][]nat.PortBinding{
+			nat.Port(containerPort + "/tcp"): {{HostPort: hostPort}},
+		},
+	}, nil, nil, containerName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		return "", fmt.Errorf("failed to start container: %w", err)
+	}
+
+	fmt.Printf("Container %s is running in detached mode.\n", containerName)
+	return resp.ID, nil // Return the container ID
 }
